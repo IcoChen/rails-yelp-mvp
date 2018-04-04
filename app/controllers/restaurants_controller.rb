f011e4db25c6d3df1f54f761fea3dd0ea22ad565
Ico Chen <icelandsoundsneat@outlook.com>
2018-04-04 15:45:36 +0800

    okokok

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -18,7 +18,6 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-
     respond_to do |format|
       if @restaurant.save
         format.html { redirect_to @restaurant, notice: 'Created.' }
